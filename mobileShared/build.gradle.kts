c0eb3f1161e68c6e0365aaa9530c003a85788078
Catherine Gilbert <catherine.gilbert15@gmail.com>
2022-03-15 16:50:09 -0400

    Fixed IOS build

--- mobileShared/build.gradle.kts
+++ mobileShared/build.gradle.kts
@@ -20,7 +20,8 @@ kotlin {
     android()
     iosX64()
     iosArm64()
-    iosSimulatorArm64()
+    // TODO: add this back when kotlin-result supports it
+    //iosSimulatorArm64()
 
     cocoapods {
         summary = "Some description for the Shared Module"
@@ -55,25 +56,25 @@ kotlin {
         val androidTest by getting
         val iosX64Main by getting
         val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
+        // val iosSimulatorArm64Main by getting
 
         @Suppress("UNUSED_VARIABLE")
         val iosMain by creating {
             dependsOn(commonMain)
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
+            //iosSimulatorArm64Main.dependsOn(this)
         }
         val iosX64Test by getting
         val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
+        //val iosSimulatorArm64Test by getting
 
         @Suppress("UNUSED_VARIABLE")
         val iosTest by creating {
             dependsOn(commonTest)
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
+            //iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
